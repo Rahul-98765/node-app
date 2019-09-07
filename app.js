b8ae6dbc593589bc12af10bfac881cd6fa8b5d61
Rahul <54998530+Rahul-98765@users.noreply.github.com>
2019-09-07 09:56:48 +0530

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+
+const port = process.env.PORT || 9000;
+
+const server = http.createServer((req,res) =>{
+    console.log('server received request');
+    res.end('Hello World ');
+})
+
+server.listen(port, err =>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`server listening at port ${port}`);
+    }
+})
