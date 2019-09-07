7b16d140f26db05cf9d4e4f0a3e578dcf96fe903
Rahul <54998530+Rahul-98765@users.noreply.github.com>
2019-09-07 12:28:30 +0530

    set env port

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const http = require("http");
 
 
 const port = process.env.PORT || 9000;
+const NODE_ENV = process.env.NODE_ENV;
+const NODE_TEST = process.env.NODE_TEST
 
 const server = http.createServer((req,res) =>{
     console.log('server received request');
-    res.end(`Hello World at port ${port}`);
+    res.end(`Hello World at port ${port} ${NODE_ENV} ${NODE_TEST}`);
 })
 
 server.listen(port, err =>{
